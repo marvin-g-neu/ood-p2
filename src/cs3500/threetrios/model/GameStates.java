60204d7ecb628e7cdb52c7787b8500ec19396f0a
marvin-g-neu <141779828+marvin-g-neu@users.noreply.github.com>
2024-10-28 00:47:18 -0400

    created game states enum for model and moved player interface design doc to /docs

---
+++ src/cs3500/threetrios/model/GameStates.java
@@ -0,0 +1,10 @@
+package cs3500.threetrios.model;
+
+/**
+ * Keeps track of different variables determining 
+ * the state of the game in the Three Trios game 
+ * inside the model.
+ */
+public enum GameStates {
+    // TK...
+}
