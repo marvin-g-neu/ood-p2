4aa8ba6d2ebd5fa34b68135ee26595d3e99d0ddb
batkojed <jmamrine@gmail.com>
2024-10-28 17:49:45 -0400

    Finish grid interface docs and ThreeTriosBoard implementations

--- src/cs3500/threetrios/model/grid/Grid.java
+++ src/cs3500/threetrios/model/grid/Grid.java
@@ -2,7 +2,7 @@ package cs3500.threetrios.model.grid;
 
 import cs3500.threetrios.model.card.CustomCard;
 import cs3500.threetrios.model.cell.Cell;
-import cs3500.threetrios.model.card.ThreeTriosCard;
+
 /**
  * Represents a game board grid for Three Trios.
  * The grid consists of cells that can either be holes or spaces for cards.
@@ -14,7 +14,7 @@ public interface Grid {
    * @param row the row index
    * @param col the column index
    * @return the cell at the specified position
-   * @throws IllegalArgumentException if the position is invalid
+   * @throws IllegalArgumentException if the row or column is out of range
    */
   Cell getCell(int row, int col);
 
@@ -22,10 +22,12 @@ public interface Grid {
    * Places a card at the specified position.
    *
    * @param card the card to place
-   * @param row the row index
-   * @param col the column index
-   * @throws IllegalArgumentException if the position is 
-   * invalid or cell cannot accept card
+   * @param row  the row index
+   * @param col  the column index
+   * @throws IllegalArgumentException if the card is null
+   * @throws IllegalArgumentException if the card color is UNASSIGNED
+   * @throws IllegalArgumentException if the row or column is out of range
+   * @throws IllegalStateException    if the cell is not empty or is a hole
    */
   void placeCard(CustomCard card, int row, int col);
 
@@ -63,7 +65,7 @@ public interface Grid {
    * @param row the row index
    * @param col the column index
    * @return array of adjacent cells in order [north, south, east, west], null if no adjacent cell
-   * @throws IllegalArgumentException if the position is invalid
+   * @throws IllegalArgumentException if the row or column is out of range
    */
   CustomCard[] getAdjacentCards(int row, int col);
 }
