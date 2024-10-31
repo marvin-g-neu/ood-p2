d076e02548184cef44ec30b8f829c65a24baf194
batkojed <jmamrine@gmail.com>
2024-10-31 19:14:42 -0400

    Fix more exceptions

--- test/cs3500/threetrios/controller/readers/DeckFileReaderTest.java
+++ test/cs3500/threetrios/controller/readers/DeckFileReaderTest.java
@@ -1,12 +1,8 @@
 package cs3500.threetrios.controller.readers;
 
-import cs3500.threetrios.model.PlayerColor;
 import cs3500.threetrios.model.card.AttackValue;
-import cs3500.threetrios.model.card.CardColor;
 import cs3500.threetrios.model.card.CustomCard;
 import cs3500.threetrios.model.card.Direction;
-import cs3500.threetrios.model.cell.Cell;
-import cs3500.threetrios.model.cell.CellState;
 import org.junit.Test;
 
 import java.util.List;
@@ -15,22 +11,30 @@ import static org.junit.Assert.*;
 public class DeckFileReaderTest {
   @Test
   public void readFileThrowsIAEWhenFileCannotBeRead() {
-    assertThrows(IllegalArgumentException.class, () -> new GridFileReader().readFile(
-        "docs/boards/fakePath.config"));
-    assertThrows(IllegalArgumentException.class, () -> new GridFileReader().readFile(
-        "docs/cards/AllNecessaryCards.config"));
+    assertThrows(IllegalArgumentException.class, () -> new DeckFileReader().readFile(
+        "docs/cards/fakePath.config"));
+    assertThrows(IllegalArgumentException.class, () -> new DeckFileReader().readFile(
+        "docs/boards/boardWithNoUnreachableCardCells.config"));
   }
 
   @Test
   public void readFileGetsCorrectList() {
-    GridFileReader gridFileReader = new GridFileReader();
-    Cell[][] grid = gridFileReader.readFile("docs/boards/boardWithNoUnreachableCardCells.config");
+    DeckFileReader deckFileReader = new DeckFileReader();
+    List<CustomCard> cards = deckFileReader.readFile("docs/cards/AllNecessaryCards.config");
 
-    assertEquals(4, grid.length);
-    assertEquals(7, grid[0].length);
-    Cell firstCell = grid[0][0];
-    Cell lastCell = grid[grid.length - 1][grid[0].length - 1];
-    assertEquals(CellState.EMPTY, firstCell.getCellState());
-    assertEquals(CellState.HOLE, lastCell.getCellState());
+    assertEquals(36, cards.size());
+    CustomCard firstCard = cards.get(0);
+    assertEquals("DarkMagician",firstCard.getName());
+    assertEquals(AttackValue.A, firstCard.getAttackValue(Direction.NORTH));
+    assertEquals(AttackValue.SIX, firstCard.getAttackValue(Direction.SOUTH));
+    assertEquals(AttackValue.SEVEN, firstCard.getAttackValue(Direction.EAST));
+    assertEquals(AttackValue.SEVEN, firstCard.getAttackValue(Direction.WEST));
+
+    CustomCard lastCard = cards.get(35);
+    assertEquals("MagicianOfBlackChaos",lastCard.getName());
+    assertEquals(AttackValue.EIGHT, lastCard.getAttackValue(Direction.NORTH));
+    assertEquals(AttackValue.A, lastCard.getAttackValue(Direction.SOUTH));
+    assertEquals(AttackValue.NINE, lastCard.getAttackValue(Direction.EAST));
+    assertEquals(AttackValue.SEVEN, lastCard.getAttackValue(Direction.WEST));
   }
 }
